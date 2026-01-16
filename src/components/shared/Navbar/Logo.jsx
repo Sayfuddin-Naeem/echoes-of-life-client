c6092dad1573001850be9ed9b7f29315ec40b733
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2026-01-16 22:08:52 +0600

    feat: create navbar logo

---
+++ src/components/shared/Navbar/Logo.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+import NavbarLogo from "../Logoes/NavbarLogo";
+
+const Logo = () => {
+  return (
+    <Link to="/" className="flex items-center gap-2 text-xl font-bold">
+      <NavbarLogo />
+      <span className="hidden sm:inline font-serif-4">Echoes of Life</span>
+      <span className="sm:hidden font-serif-4">EOL</span>
+    </Link>
+  );
+};
+
+export default Logo;
