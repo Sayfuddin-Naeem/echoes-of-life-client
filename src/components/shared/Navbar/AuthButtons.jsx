ef787f385530152e438a511bb35a97bd334217f1
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2026-01-16 22:07:06 +0600

    feat: create authbuttons components for signup and singin

---
+++ src/components/shared/Navbar/AuthButtons.jsx
@@ -0,0 +1,41 @@
+import { LogIn, UserPlus } from "lucide-react";
+import { Link } from "react-router";
+
+// Auth Buttons Component
+const AuthButtons = ({ isMobile, onLinkClick }) => {
+  if (isMobile) {
+    return (
+      <div className="flex flex-col gap-2 px-4 py-3">
+        <Link
+          to="/login"
+          className="btn btn-ghost justify-start"
+          onClick={onLinkClick}
+        >
+          <LogIn className="w-5 h-5" />
+          Login
+        </Link>
+        <Link
+          to="/signup"
+          className="btn btn-primary justify-start"
+          onClick={onLinkClick}
+        >
+          <UserPlus className="w-5 h-5" />
+          Sign Up
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      <Link to="/login" className="btn btn-ghost">
+        Login
+      </Link>
+      <Link to="/signup" className="btn btn-primary">
+        Sign Up
+      </Link>
+    </div>
+  );
+};
+
+export default AuthButtons;
